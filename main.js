8f9accbf1886d7aa651b4fa473177f32e916a6cf
Francisco159 <javier.671634@gmail.com>
2021-08-16 09:00:26 -0500

    Mis diseños incluyen cambios en archivos html, css y js

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-console.log("Imprimir mensajes en consola");
+console.log("Imprimir en consola");
 
-//alert("Imprimir mensaje en una ventana");
+//alert("Imprime mensaje en una ventana")
 
-var x = 0;
-var name = "Luz";
-
+var x = 3;
+var name = "Francisco";
 var ciertoFalso = true;
-
-var myArray = ["Luz", "Luis Eduardo", "Leo", "Melissa"];
+var myArray = ["Luz", "Francisco", "Gonzalez"];
 
 var body = document.getElementsByTagName("body");
 
 var row = document.getElementsByClassName("row");
 
-//console.log(x + " " + name);
-
-ciertoFalso = false;
-
-function imprimirMensaje(){
-    for(var i = 0; i <  row.length; i++) {
-        row[i].classList.add("row-" +i);
+function imprimirMensaje() {
+    for (let i = 0; i < row.length; i++) {
+        row[i].classList.add("row-" + i);
         console.log(row[i]);
     }
 }

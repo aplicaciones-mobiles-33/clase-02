a2c0007709f1ffed8974056587bad8390012f2c8
Ivrohm <72846342+Ivrohm@users.noreply.github.com>
2021-08-17 21:28:03 -0500

    Clase 02 - Repaso general de Programación Web + Git
    Se cambio el diseño de pagina y algunos elementos, dados en la tarea

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var name = "Luz";
 
 var ciertoFalso = true;
 
-var myArray = ["Luz", "Luis Eduardo", "Leo", "Melissa"];
+var myArray = ["Luz", "Juan", "Leo", "Melissa"];
 
 var body = document.getElementsByTagName("body");
 
